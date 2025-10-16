d670a2a608605f9f456c055e99db41cd270ab4e7
trung <trung@hydrax.io>
2025-10-16 08:45:46 +0000

    update loader

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,7 @@
 import mergeEntries from "../processors/merge";
 import data from "./aggregated";
 
-const addLabels = ["20251015"];
+const addLabels = ["251015"];
 
 export const labels = [...data.map((item) => item.time), ...addLabels];
 
@@ -13,24 +13,22 @@ const mergeNotes = (inputs: Array<RawEntry>) =>
     ])
   );
 
-// export const loadData2 = () =>
-//   Promise.all(
-//     addLabels.map((label) =>
-//       import(`./20${label}.js`).then((module) => {
-//         const record: RawEntry = Array.isArray(module.default.entries)
-//           ? module.default
-//           : { entries: module.default };
-//         record.time = label;
-//         return record;
-//       })
-//     )
-//   ).then((entries) => {
-//     entries = [...data, ...entries];
-//     return [mergeEntries(entries), mergeNotes(entries)] as const;
-//   });
+const loadNewData = () =>
+  Promise.all(
+    addLabels.map((label) =>
+      import(`./20${label}.ts`).then((module) => {
+        const record: RawEntry = Array.isArray(module.default.entries)
+          ? module.default
+          : { entries: module.default };
+        record.time = label;
+        return record;
+      })
+    )
+  );
 
 export const loadData = async () => {
-  return [mergeEntries(data), mergeNotes(data)] as const;
+  const newData = [...data, ...(await loadNewData())];
+  return [mergeEntries(newData), mergeNotes(newData)] as const;
 };
 
 // https://www.scymed.com/en/smnxps/psxkc035_c.htm
