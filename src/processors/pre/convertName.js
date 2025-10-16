1eef653669749f460f33c5fd236ea1eb655818cc
trung <trung@hydrax.io>
2025-10-16 15:23:19 +0000

    update loader

--- src/processors/pre/convertName.js
+++ src/processors/pre/convertName.js
@@ -50,6 +50,7 @@ const nameMapper = {
 
 const keys = Object.keys(nameMapper);
 export default ([name, values, unit, extra]) => {
-  const key = keys.find(k => name.startsWith(k))
+  const key = keys.find((k) => name.startsWith(k));
+  console.log(key);
   return [nameMapper[key] || name, values, unit, extra];
 };
